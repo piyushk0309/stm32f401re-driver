10de831b4379fafcc42721847c3cab1e40191c8a
piyushk0309 <piyushkamra37@gmail.com>
2026-06-11 21:23:32 +0530

    stm32f401re_i2c_driver.h

---
+++ Inc/stm32f401xx_i2c_driver.h
@@ -0,0 +1,139 @@
+/*
+ * stm32f401xx_i2c_driver.h
+ *
+ *  Created on: 05-Jun-2026
+ *      Author: hp
+ */
+
+#ifndef INC_STM32F401XX_I2C_DRIVER_H_
+#define INC_STM32F401XX_I2C_DRIVER_H_
+
+#include "stm32f401xx.h"
+
+
+
+
+
+
+//config structure of i2c peripheral
+
+
+typedef struct
+{
+	uint32_t  I2CSCLSpeed;
+	uint8_t	  I2CDeviceAddress;
+	uint8_t	  I2C_ACKControl;
+	uint16_t  I2C_FMDutyCycle;
+
+}I2C_Config_t;
+
+#endif /* INC_STM32F401XX_I2C_DRIVER_H_ */
+
+
+//handle structure for i2c peripheral
+
+typedef struct
+{
+	I2C_Reg_Def_t  *pI2Cx;
+	I2C_Config_t   I2C_Config;
+	uint8_t		   *pTxBuffer;
+	uint8_t		   *pRxBuffer;
+	uint32_t 		TxLen;
+	uint32_t 		RxLen;
+	uint8_t			TxRxState;
+	uint8_t			DevAddr;
+	uint32_t		RxSize;
+	uint8_t			Sr;  // TO STORE REPEATED START VALUE
+
+
+
+
+}I2C_Handle_t;
+
+
+//I2C APPLICATION STATES
+#define I2C_READY			0
+#define I2C_BUSY_IN_RX		1
+#define I2C_BUSY_IN_TX		1
+
+
+
+
+
+//@I2C_SCLSpeed
+#define I2C_SCL_SPEED_SM	1000000
+#define I2C_SCL_SPEED_FM2K	2000000
+#define I2C_SCL_SPEED_FM4K	4000000
+
+
+
+
+//@I2C_ACKControl
+#define I2C_ACK_ENABLE		1
+#define I2C_ACK_DISABLE		0
+
+
+//@I2C_FMDutyCycle
+#define I2C_FM_DUTY_2		0
+#define I2C_FM_DUTY_16_9	1
+
+
+
+/* PERIPHERAL CLOCK SETUP*/
+
+void I2C_PeriClockControl(I2C_Reg_Def_t *pI2Cx,uint8_t EnorDi);
+
+/*INIT AND DE-INIT*/
+
+void I2C_Init(I2C_Handle_t *pI2CHandle);
+void I2C_DeInit(I2C_Reg_Def_t *pI2Cx);
+
+//data send and receive WITH POLLING
+void I2C_MasterSendData(I2C_Handle_t *pI2CHandle,uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr);
+void I2C_MasterRecieveData(I2C_Handle_t *pI2CHandle,uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr);
+
+void I2C_SlaveSendData(I2C_Reg_Def_t *pI2Cx,uint8_t Data);
+uint8_t I2C_SlaveRecieveData(I2C_Reg_Def_t *pI2Cx);
+
+//data send and receive WITH INTERRUPT
+uint8_t I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
+uint8_t I2C_MasterRecieveDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
+
+//I2C IRQ CONFIG
+void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void I2C_IRQPriorityConfig(uint8_t IRQNumber,uint32_t Priority );
+void I2C_EV_IRQHandling(I2C_Handle_t *pI2CHandle);
+void I2C_ER_IRQHandling(I2C_Handle_t *pI2CHandle);
+
+//I2C APPLICATION EVENTS MACROS '
+#define I2C_EV_TX_CMPLT			0
+#define I2C_EV_RX_CMPLT			1
+#define I2C_EV_STOP				2
+#define I2C_ERROR_BERR 			3
+#define I2C_ERROR_ARLO  		4
+#define I2C_ERROR_AF    		5
+#define I2C_ERROR_OVR   		6
+#define I2C_ERROR_TIMEOUT 		7
+#define I2C_EV_DATA_RCV			8
+#define I2C_EV_DATA_REQ			9
+
+//SR MACROS
+#define I2C_DISABLE_SR	1
+#define I2C_ENABLE_SR	0
+
+//tx rx closing apis
+void I2C_CloseSendData(I2C_Handle_t *pI2CHandle);
+void I2C_CloseRecieveData(I2C_Handle_t *pI2CHandle);
+
+
+
+// other apis
+
+void I2C_PeripheralControl(I2C_Reg_Def_t *pI2Cx, uint8_t ENorDI);
+
+
+void I2C_ApplicationEventCallback(I2C_Handle_t *I2C_Handle_t, uint8_t AppEv);
+
+
+
+
